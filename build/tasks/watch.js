5115137ddb60b9e7c37517d57abefeb8416d2ba0
Eric Peeters <eric.peeters@redhotminute.com>
2016-09-21 16:18:03 +0200

    Added strict mode to watch task and fixed indentation

--- build/tasks/watch.js
+++ build/tasks/watch.js
@@ -1,6 +1,8 @@
 module.exports = function watchTask(gulp, plugins, config) {
-    return gulp.task(
-        'watch',
-        plugins.taskManager.getWatchTasks()
-    );
+	'use strict';
+
+	return gulp.task(
+		'watch',
+		plugins.taskManager.getWatchTasks()
+	);
 };
